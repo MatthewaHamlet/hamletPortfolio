70fb59f0d58799f2ecababd882254e8f7ce94cc8
Matthew Hamlet <matthewahamlet@gmail.com>
2019-03-06 11:19:08 -0700

    added Webpack-CLI Bootstrap jQuery popper.js setup webpackconfig from multipage static site. Animation landing page done and responsive.

---
+++ src/app.js
@@ -0,0 +1,6 @@
+require ('./js/main');
+require ('./scss/style.scss');
+import 'bootstrap';
+
+
+console.log('this is the index page');
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+
+
+module.exports = {
+    entry: {
+       index: './src/app.js',
+    },
+    output: {
+        path: __dirname + '/dist/build',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'src'),
+        port: 8800,
+        watchContentBase: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                loaders: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                  'style-loader',
+                  { loader: 'css-loader', options: { importLoaders: 1 } },
+                  'postcss-loader'
+                ]
+              }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename:'index.html',
+            template: 'src/index.html'
+        }),
+        new HtmlWebpackPlugin({
+            filename:'landing.html',
+            template: 'src/landing.html',
+            chunks: []
+        })
+    ]
+}
